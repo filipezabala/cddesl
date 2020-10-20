81c07e218fe7eb3f41be813e0b0c10d60aba6e17
Filipe Zabala <filipezabala@MacBook-Air-de-Filipe.local>
2020-10-20 00:09:14 -0300

    Atualização das informações.

--- codigos/DS6_Prob.R
+++ codigos/DS6_Prob.R
@@ -1,25 +1,34 @@
 ################################################
-###  Uma Introdução à Ciência de dados no R  ###
-###              Filipe J. Zabala            ###
-###        www.estatisticaclassica.com       ###
-###           filipe.zabala@pucrs.br         ###
-###           2018-11-12 a 2018-11-21        ###
+###    Ciência de dados em software livre    ###
+###         http://filipezabala.com          ###
+###  https://github.com/filipezabala/cddesl  ###
+###            Início: 2020-10-11            ###
+###      Última atualização: 2020-10-20      ###
 ################################################
 
-### Referências
+# Playlist
+# https://www.youtube.com/playlist?list=PLgnUrXr7_7coSfm067nFXPvShO18o6GQ_
+
+### Textos
 # https://cran.r-project.org/doc/manuals/r-release/R-intro.pdf
 # http://r4ds.had.co.nz/
-# http://www.estatisticaclassica.com
+# https://www.curso-r.com/
+# https://www.ufrgs.br/wiki-r
 # http://www.burns-stat.com/pages/Tutor/R_inferno.pdf
+# https://www.amazon.com/ggplot2-Elegant-Graphics-Data-Analysis/dp/331924275X/
+
+### Formulários e resumos
 # https://www.rstudio.com/wp-content/uploads/2016/10/r-cheat-sheet-3.pdf
 # https://www.rstudio.com/wp-content/uploads/2016/01/rstudio-IDE-cheatsheet.pdf
 # https://www.causascientia.org/math_stat/Dists/Compendium.pdf
-# https://www.amazon.com/ggplot2-Elegant-Graphics-Data-Analysis/dp/331924275X/
-# https://www.r-graph-gallery.com/
-# https://www.shinyapps.org/apps/RGraphCompendium/index.php
-# https://www.curso-r.com/
-# https://www.ufrgs.br/wiki-r
 
+### Gráficos
+# https://plot.ly/r/
+# https://www.r-graph-gallery.com/
+# https://github.com/d3/d3/wiki/Gallery
+# http://kateto.net/network-visualization
+# https://www.shinyapps.org/apps/RGraphCompendium/index.php
+# http://r-statistics.co/Top50-Ggplot2-Visualizations-MasterList-R-Code.html
 
 ### Links para a instalação do R e RStudio
 # https://cloud.r-project.org/
@@ -27,18 +36,21 @@
 
 
 ### Tópicos
-# 1 Funções básicas do R
-# 2 Criando e manipulando funções
-# 3 Manipulando dados com dplyr and tidyr
-# 4 Estatística	descritiva, visualização e séries temporais
-# 5 Probabilidade  <-- 
-# 6 Inferência
-# 7 Tópicos em Modelos Lineares Generalizados
-# 8 Aprendizagem de máquina
+# 0 A primeira seção de R e RStudio
+# 1 Funções básicas do R e RStudio 
+# 2 Objetos e funções úteis
+# 3 Criando e manipulando funções
+# 4 Manipulando dados com dplyr and tidyr
+# 5 Estatística	descritiva, visualização e séries temporais
+# 6 Probabilidade  <-- 
+# 7 Inferência
+# 8 Tópicos em Modelos Lineares Generalizados
+# 9 Aprendizagem de máquina
+
 
 
 #######################
-### 5 Probabilidade ###
+### 6 Probabilidade ###
 #######################
 
 # Normal ou gaussiana
