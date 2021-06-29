8775da016739690b0a9f1abca1c5c263db669f2f
Filipe Zabala <filipezabala@MacBook-Air-de-Filipe.local>
2021-06-28 22:32:49 -0300

    Updates.

--- codigos/DS1_Basico.R
+++ codigos/DS1_Basico.R
@@ -3,7 +3,7 @@
 ###         http://filipezabala.com          ###
 ###  https://github.com/filipezabala/cddesl  ###
 ###            Início: 2020-10-11            ###
-###      Última atualização: 2021-06-23      ###
+###      Última atualização: 2021-06-28      ###
 ################################################
 
 # Playlist
@@ -176,7 +176,7 @@ for(i in 6:-2){print(round(153.456789,i))}
 options(digits = 9) # Ajusta apresentação para 9 dígitos (padrão é 7)
 for(i in 6:-2){print(round(153.456789,i))}
 
-# sprintf do C
+# sprintf da linguagem C
 sprintf('%f', pi)
 sprintf('%.3f', pi)
 sprintf('%.200f', pi)
@@ -188,6 +188,7 @@ sprintf('%+f', pi)
 sprintf('% f', pi)
 sprintf('%-10f', pi) # left justified
 sprintf('%e', pi)
+sprintf('%.3e', pi)
 sprintf('%E', pi)
 sprintf('%g', pi)
 sprintf('%g',   1e6 * pi) # -> exponential
@@ -205,5 +206,90 @@ sprintf('%G', 1e-6 * pi)
 as.numeric(c(T,F,F,T,T,T,T,F,T))  # conversão para numérico
 sum(c(T,F,F,T,T,T,T,F,T))
 ?base::Logic  # ajuda de 'Logical Operators'
+
+
+# Valores faltantes (NA/missing values)
+(z1 <- c(1:3, NA))    # NA é um símbolo especial
+(z2 <- c(1:3, NB))    # erro, pois 'NB' não é símbolo especial
+sum(is.na(z1))
+mean(z1)
+mean(z1, na.rm = T)
+
+
+# Diretório de trabalho (working directory)
+getwd()        # Apresenta o diretório corrente
+setwd('~/')   # Ajusta o diretório corrente
+dir()          # Apresenta o conteúdo do diretório corrente
+
+(x <- 1:10)
+rev(x)          # reverte o vetor
+cumsum(x)       # soma acumulada
+cumsum(rev(x))  # soma acumulada inversa
+
+(x <- c(3:5, 11:8, 8 + 0:5))
+(ux <- unique(x))           # apresenta uma lista de valores distintos
+length(x)                   # 13 elementos, contando as repeticoes
+length(unique(x))           # 9 elementos únicos
+duplicated(x)               # indica as posicoes onde há duplicações
+
+(tab <- table(duplicated(x)))     # cria uma tabela
+prop.table(tab)                   # proporções
+linha <- sample(c('a','b'), 10, replace = T)
+coluna <- sample(c('c','d'), 10, replace = T)
+(tab2 <- table(linha, coluna))     # tabela cruzada
+margin.table(tab2, 1)       # soma marginal na linha (margin=1)
+margin.table(tab2, 2)       # soma marginal na coluna (margin=2)
+margin.table(tab2)          # soma marginal total
+addmargins(tab2)
+
+match(1:10, c(1,9,8,4))     # retorna um vetor das posições do primeiro objeto que corresponde com o segundo
+1:10 %in% c(1,9,8,4)        # retorna TRUE onde há correspondência e FALSE caso contrário
+
+(r1 <- rank(x1 <- c(3,1,4,15,92)))    # indica a ordem de cada elemento do vetor
+x2 <- c(3,1,4,1,5,9,2,6,5,3,5)
+names(x2) <- letters[1:length(x2)]
+(r2 <- rank(x2))                          # ties.method = c('average', 'first', 'last', 'random', 'max', 'min')
+
+sample(1:60, size=6)                      # amostra pseudo-aleatoria para jogar na mega-sena
+set.seed(20); sample(1:60, size=6)        # ajustando a semente pseudo-aleatoria
+set.seed(20); sort(sample(1:60, size=6),decreasing=T)  # ordenando, para facilitar a leitura
+
+letters                       # letras minusculas
+LETTERS                       # letras maiusculas
+LETTERS[1]                    # primeira posição de LETTERS
+noquote(letters)              # sem aspas
+
+substr('abcdef', 2, 4)        # apresenta da segunda ate a quarta posição
+
+x <- c('Tche', 'Churrasco', 'Chimarrao', 'Farrapo', 'Gaita', 'Bah')
+strsplit(x, 'a')              # retira a letra 'a'
+
+tolower(x)    # minusculas
+toupper(x)    # maiusculas
+
+texto <- 'Rio Grande do Sul'
+nchar(texto)    # conta o número de caracteres, contando os espaços
+stringi::stri_stats_latex(texto)  # relatório mais completo
+
+x <- c(2,4,-2,6,7,8)
+diff(x)           # tira a diferenca, note que restam n-1 elementos
+range(x)          # apresenta o mínimo e o máximo
+diff(range(x))    # tira a diferenca entre o mínimo e o máximo (amplitude)
+
+as.Date('11/3/1981', format = '%d/%m/%Y')   # coloca no formato de data...
+as.Date('3:11:1981', format = "%m:%d:%Y")   # ... não importa o separador
+(data <- as.Date('11/3/1981', format='%d/%m/%Y'))
+class(data)       # classe Date
+months(data)      # mês
+weekdays(data)    # dia da semana
+quarters(data)    # trimestre
+Sys.Date() - data # número de dias entre hoje e data
+
+(m <- cbind(x = 1:10, y = (-4:5)^2))    
+split(m, col(m))      # divide uma matriz em uma lista pelas colunas, gerando uma lista
+split(1:20, 1:2)      # divide a sequencia de 1 a 10 de dois em dois valores (pares/ímpares)
+
+(x <- rep(1:3,3))
+car::recode(x, " c(1,2) = 'A'; else = 'B' ")  # recodifica
 
 # =)
